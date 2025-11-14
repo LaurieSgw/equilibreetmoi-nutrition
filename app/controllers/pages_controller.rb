9a388318be163746ca1c8de38af8249b9e2e152f
Laurie Segalowitch <lausegalo@gmail.com>
2025-11-14 13:07:39 +0100

    initialisation routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  def prestations
+  end
+
+  def prendrerdv
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/prestations'
+  get 'pages/prendrerdv'
+  get 'pages/contact'
+
+  root to: "pages#home"
+
+  get "prestations", to: "pages#prestations"
+  get "prendrerdv", to: "pages#prendrerdv"
+  get "contact", to: "pages#contact"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
