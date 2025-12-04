e02143e86809495234b7f13ff3dba7bfdcf704a5
Laurie Segalowitch <lausegalo@gmail.com>
2025-12-04 13:48:51 +0100

    form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,21 @@ class PagesController < ApplicationController
   end
 
   def prendrerdv
+    @form = Form.new
+
+    def create
+    @form = Form.new(params[:form])
+      @form.request = request
+      if @form.deliver
+        flash.now[:success] = 'Message envoyé !'
+      else
+        flash.now[:error] = 'Could not send message'
+        render :prendrerdv
+      end
+    end
   end
 
   def contact
   end
+
 end
