474b19de2bc9c9d886723434a480f699a0d9ebd4
Laurie Segalowitch <lausegalo@gmail.com>
2026-01-09 12:12:39 +0100

    Fix SMTP production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,14 +36,16 @@ Rails.application.configure do
   config.assets.compile = false
 
   config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
-  address: ENV["smtp-relay.brevo.com"],
-  port: ENV["587"],
+  address: "smtp-relay.brevo.com",
+  port: 587,
+  authentication: :login,
   user_name: ENV["SMTP_USERNAME"],
   password: ENV["SMTP_PASSWORD"],
-  authentication: :login,
   enable_starttls_auto: true
 }
+
 
  config.action_mailer.default_url_options = { host: 'immense-garden-16196.herokuapp.com' }
 
