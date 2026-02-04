bacdc4d27a001c7df397db506c8979c4be2dddcc
Laurie Segalowitch <lausegalo@gmail.com>
2026-02-04 20:31:55 +0100

    responsive updates mobile version

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", function () {
+  const burger = document.getElementById("burger");
+  const menu = document.getElementById("nav-links");
+
+  if (!burger || !menu) return;
+
+  burger.addEventListener("click", function () {
+    menu.classList.toggle("active");
+  });
+});
